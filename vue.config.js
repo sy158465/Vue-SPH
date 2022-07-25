41ff3ccbc695e2e3340563db1939f4ed65b6fe2b
sy158465 <sy158465@qq.com>
2022-07-25 15:20:38 +0800

    v

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  //生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // Vue 兼容 IE,可以启用本选项，以避免构建后的代码中出现未转译的第三方依赖
   transpileDependencies: true,
   // 关闭eslint校验工具
-  lintOnSave:false,
+  lintOnSave: false,
   // 配置代理跨域
-  devServer:{
-    proxy:{
-      "/api":{
-        target:"http://gmall-h5-api.atguigu.cn"
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://gmall-h5-api.atguigu.cn"
       }
     }
   }
